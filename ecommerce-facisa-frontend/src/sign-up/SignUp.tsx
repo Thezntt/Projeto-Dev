8b4565af8bc3cec61c59a54879e1bee7e66f6df0
Breno Barbosa <brenobarbosa117@gmail.com>
2025-12-05 22:04:13 -0300

    Consertando Endpoint

--- ecommerce-facisa-frontend/src/sign-up/SignUp.tsx
+++ ecommerce-facisa-frontend/src/sign-up/SignUp.tsx
@@ -21,7 +21,7 @@ export default function SignUp() {
 
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:3000/api/user/signup', {
+            const response = await fetch('http://localhost:3000/api/users/signup', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, email, password, cpf }),
